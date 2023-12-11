dc86368a38fb50cbdeba74afe2905c14ee3ab732
vlopatka <vadim.lopatka@gmail.com>
2023-12-11 19:10:22 +0200

    added TrailingSlashRemover multi-slash handling

--- src/TrailingSlashRemover.kt
+++ src/TrailingSlashRemover.kt
@@ -1,10 +1,13 @@
 package app.bp
 
+import klite.BadRequestException
 import klite.Before
 import klite.HttpExchange
 
 class TrailingSlashRemover : Before {
     override suspend fun before(exchange: HttpExchange) {
+        if (exchange.path.endsWith("//"))
+            throw BadRequestException("Can't parse the path: ${exchange.path}")
         if (exchange.path.endsWith("/"))
             exchange.redirect(exchange.path.substring(0, exchange.path.length - 1))
     }
--- test/TrailingSlashRemoverTest.kt
+++ test/TrailingSlashRemoverTest.kt
@@ -1,9 +1,11 @@
 import app.bp.TrailingSlashRemover
+import ch.tutteli.atrium.api.fluent.en_GB.messageToContain
 import ch.tutteli.atrium.api.fluent.en_GB.toThrow
 import ch.tutteli.atrium.api.verbs.expect
 import io.mockk.every
 import io.mockk.mockk
 import io.mockk.verify
+import klite.BadRequestException
 import klite.HttpExchange
 import klite.RedirectException
 import kotlinx.coroutines.runBlocking
@@ -16,6 +18,13 @@ class TrailingSlashRemoverTest {
     @Test fun `should do nothing when trailing slash is absent`() {
         every { exchange.path } returns "/hello"
         runBlocking { trailingSlashRemover.before(exchange) }
+        verify(exactly = 0) { exchange.redirect(any<String>()) }
+    }
+
+    @Test fun `should throw 400 BadRequest when several trailing slashes`() {
+        every { exchange.path } returns "/hello//"
+        expect { runBlocking { trailingSlashRemover.before(exchange) }}.toThrow<BadRequestException>()
+            .messageToContain("Can't parse the path: /hello//")
         verify(exactly = 0) { exchange.redirect(any<String>()) }
     }
 

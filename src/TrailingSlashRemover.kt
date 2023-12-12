8f2362b87603847ed1f9da27a13b7518da6bbcac
vlopatka <vadim.lopatka@gmail.com>
2023-12-12 10:16:59 +0200

    use when instead of if in TrailingSlashRemover

--- src/TrailingSlashRemover.kt
+++ src/TrailingSlashRemover.kt
@@ -5,10 +5,10 @@ import klite.Before
 import klite.HttpExchange
 
 class TrailingSlashRemover : Before {
-    override suspend fun before(exchange: HttpExchange) {
-        if (exchange.path.endsWith("//"))
-            throw BadRequestException("Can't parse the path: ${exchange.path}")
-        if (exchange.path.endsWith("/"))
-            exchange.redirect(exchange.path.substring(0, exchange.path.length - 1))
+    override suspend fun before(ex: HttpExchange) {
+        when {
+            ex.path.endsWith("//") -> throw BadRequestException("Can't parse the path: ${ex.path}")
+            ex.path.endsWith("/") -> ex.redirect(ex.path.substring(0, ex.path.length - 1))
+        }
     }
 }
